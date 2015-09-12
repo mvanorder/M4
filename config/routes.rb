90710911c8b784cbc89c906105ffd49844d3ea43
Malcolm VanOrder <mvanorder1390@gmail.com>
2015-09-12 15:31:55 -0400

    Created controller for users
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'welcome/index'
 
+  get 'signup' => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
