82a059c7c94d3caf10f8ebbd89535bceb9656e82
Malcolm VanOrder <mvanorder1390@gmail.com>
2015-09-12 21:52:39 -0400

    Created sessions, updated new user form, and updated the nav for users/sessions
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
+      flash[:success] = "Welcome to M4!"
       redirect_to @user
     else
       render 'new'
