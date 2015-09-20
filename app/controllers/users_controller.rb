afdf2479e40952df2341b36b2c615371331af2b8
Malcolm VanOrder <mvanorder1390@gmail.com>
2015-09-19 20:10:39 -0400

    updated styling with background
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,6 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    binding.pry
     if @user.update_attributes(user_params)
       #redirect_to @user
       flash.now[:success] = "Successfully updated profile"
